0735f59737b07b73c812540618875b84a87e9dc5
Matheus Kipper <matheuskipper9@gmail.com>
2025-04-04 09:14:12 -0300

    último commit antes de preparar o deploy

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -33,18 +33,18 @@ function Calculadora() {
   }
   return (
     <>
-      <div className="flex flex-col gap-4 bg-white p-4 rounded-md shadow-md w-full max-w-md mx-auto mb-8 mt-8">
-        <h3 className="">Calcule aqui:</h3>
-        <label>Valor Base (R$): </label>
+      <div className="flex flex-col gap-6 bg-white p-8 rounded-lg shadow-lg w-full max-w-lg mx-auto mb-12 mt-12 min-h-[400px]">
+        <h3 className="text-x1 font-bold">Calcule aqui:</h3>
+        <label className="text-left font-medium">Valor Base (R$): </label>
         <input
-          className="border-2 border-gray-300 rounded-md p-1 focus:outline-none focus:border-blue-500"
+          className="border-2 border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
           type="number"
           value={num1}
           onChange={(e) => setNum1(e.target.value)}
         />
-        <label>Selecione o tributo: </label>
+        <label className="text-left font-medium">Selecione o tributo: </label>
         <select
-          className="border-2 border-gray-300 rounded-md p-1 focus:outline-none focus:border-blue-500"
+          className="border-2 border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
           value={operacao}
           onChange={(e) => setOperacao(e.target.value)}
         >
@@ -56,7 +56,7 @@ function Calculadora() {
 
         <button
           className="bg-[#f0ce40] hover:bg-[#d3b538] border-none cursor-pointer text-white 
-          font-bold py-2 px-4 rounded"
+          font-bold py-3 px-6 rounded"
           onClick={calcular}
           type="submit"
         >
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-[#b8b7b7] text-[#252525] text-center py-3 mt-auto bottom-0 w-full">
-      <p>&copy; 2025 React Recap - Matheus Kipper</p>
+      <p>&copy; 2025 Desenvolvido por Matheus Kipper</p>
     </footer>
   );
 }
